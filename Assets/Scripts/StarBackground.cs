137e6126e96fe3651d6e2600ea4f09fb133a64a7
Penjami <rpenjami@gmail.com>
2020-01-31 22:28:33 +0200

    Star particle effect

--- Assets/Scripts/StarBackground.cs
+++ Assets/Scripts/StarBackground.cs
@@ -7,34 +7,39 @@ public class StarBackground : MonoBehaviour
     [SerializeField] private GameObject starPrefab;
 
     private List<GameObject> _stars = new List<GameObject>();
+    private List<GameObject> _removeStars = new List<GameObject>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(SpawnStars());
-        StartCoroutine(MoveStars());
-    }
 
-    IEnumerator SpawnStars() {
-        while (true) {
-            var star = Instantiate(starPrefab, new Vector3(0,Random.Range(-40f, 40f),0), Quaternion.identity);
-            _stars.Add(star);
-            yield return new WaitForSeconds(Random.Range(0.1f, 0.3f));;
+    private float _timer;
+    private float _starSpawnTime;
+
+    void Update() {
+        _timer += Time.deltaTime;
+        if(_timer > _starSpawnTime) {
+            SpawnStars();
+            _timer = 0;
+            _starSpawnTime = Random.Range(0.1f, 0.3f);
         }
+        MoveStars();
     }
 
-    IEnumerator MoveStars() {
-        var movement = new Vector3(-0.2f,0,0);
-        while (true) {
-            foreach (var star in _stars) {
-                star.transform.position -= movement;
-                if (star.transform.position.x > 200) {
-                    _stars.Remove(star);
-                    Destroy(star);
-                }
+    void SpawnStars() {
+        var star = Instantiate(starPrefab, new Vector3(0,Random.Range(-40f, 40f), Random.Range(0f, 100f)), Quaternion.identity);
+        _stars.Add(star);
+    }
+
+    void MoveStars() {
+        var movement = new Vector3(10f,0,0);
+        foreach (var star in _stars) {
+            star.transform.position -= movement * Time.deltaTime;
+            if (star.transform.position.x > 200) {
+                _stars.Remove(star);
+                _removeStars.Add(star);
             }
-
-            yield return null;
         }
+        foreach (var removeStar in _removeStars) {
+            Destroy(removeStar);
+        }
+        _removeStars.Clear();
     }
 }
