c3a5693b375f5670dd30fe49eaaff629b2f9ee14
Penjami <rpenjami@gmail.com>
2020-02-01 17:56:55 +0200

    lots of stuff

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
+
+    [SerializeField] private Transform _outerPlanet;
+    [SerializeField] private Transform _innerPlanet;
+
+    private Vector3 _rotDir;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rotDir = new Vector3(Random.Range(0.5f, 3f), Random.Range(3f, 10f), 0);
+
+        var renderer = _innerPlanet.GetComponent<Renderer>();
+
+        renderer.material.SetColor("Color_1", new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f)));
+        renderer.material.SetColor("Color_2", new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f)));
+        renderer.material.SetColor("Color_3", new Color(Random.Range(0f,1f),Random.Range(0f,1f),Random.Range(0f,1f)));
+        renderer.material.SetFloat("Noise_Offset", Random.Range(0f, 2000f));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R)) {
+            Start();
+        }
+        _innerPlanet.eulerAngles += new Vector3(0,10 * Time.deltaTime, 0);
     }
 }
---
+++ Assets/Scripts/SelectShipParts.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectShipParts : MonoBehaviour
+{
+
+    [SerializeField] private Material _defaultMaterial;
+    [SerializeField] private Material _selectedMaterial;
+
+
+    Renderer _oldSelected;
+    Renderer _newSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            SelectShipPart();
+        }
+    }
+
+    void SelectShipPart() {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if ( Physics.Raycast (ray,out hit,100000.0f)) {
+            _oldSelected = _newSelected;
+            if(_oldSelected != null) {
+                _oldSelected.material = _defaultMaterial;
+            }
+            _newSelected = hit.transform.GetComponent<Renderer>();
+            var color = _newSelected.material.color;
+            _newSelected.material = _selectedMaterial;
+            _newSelected.material.color = color;
+            Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+        }
+    }
+}
