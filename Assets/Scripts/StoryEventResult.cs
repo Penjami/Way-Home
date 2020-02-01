aff8e2af25879ab6d0ff907f762cb0d5fb344143
Ear <tosiear@gmail.com>
2020-02-02 01:15:34 +0200

    More and better events

---
+++ Assets/Scripts/StoryEventResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryEventResult {
+
+	public string eventText = ""; // description of event
+	public List<StoryResource> requirements = new List<StoryResource>(); // things needed for this event resolution
+	public List<StoryResource> successResources = new List<StoryResource>(); // consequences of resolution
+}
