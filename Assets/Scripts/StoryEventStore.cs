17a16fa8e2bd825e078c9cbb076aec3e28783f5d
Ear <tosiear@gmail.com>
2020-02-02 10:40:33 +0200

    eventit hukassa

---
+++ Assets/Scripts/StoryEventStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryEventStore {
+	StoryEvent defaultEvent = new StoryEvent();
+	public List<StoryEvent> storyEvents = new List<StoryEvent>();
+
+	StoryEventStore(){
+		defaultEvent.name = "Default";
+		StoryEventResult defaultResult = new StoryEventResult();
+		defaultResult.eventText = "Nothing happens. Seems that developers of this game have not implemented this event.";
+	}
+
+
+
+	public StoryEvent getEventByName(string name){
+		for(int i = 0; i < storyEvents.Count; i++){
+			if (storyEvents[i].name == name){
+				return storyEvents[i];
+			}
+		}
+		Debug.Log("Requested event " + name + " not found");
+		return defaultEvent;
+	}
+}
